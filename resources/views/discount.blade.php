3b8f4f97b454794c9ca0266d330f87c581fb2768
Maxim <ostin2605@gmail.com>
2023-07-20 17:56:24 +0300

    todo check

--- resources/views/discount.blade.php
+++ resources/views/discount.blade.php
@@ -9,12 +9,14 @@
             <h1>{{ $message }}</h1>
         @endif
 
-        <form method="get" action={{ url('discount/check') }}>
+        <form method="get" action="/discount/check" }}>
             @csrf
             <input type="text" id="code" name="code" class="form-control">
 
             <button type="submit">Проверить скидку</button>
         </form>
+
+        // alternative: https://stackoverflow.com/questions/27298426/how-to-pass-get-parameters-to-laravel-from-with-get-method
 
         @if($answer)
             <h1>{{ $answer }}</h1>
